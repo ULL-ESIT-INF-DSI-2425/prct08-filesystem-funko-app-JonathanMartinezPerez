7fa6bf5d47161b353b5fadd0b66343b458f34fb3
JonathanMartinezPerez <alu0101254098@ull.edu.es>
2025-03-22 19:26:30 +0000

    Delete command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import "./commands/add.js";
+import "./commands/delete.js";
+import "./commands/update.js";
+import "./commands/list.js";
 
 /** 
  * Yargs para gestionar todos los comandos.
  */
-yargs(hideBin(process.argv)).help().argv;
+yargs(hideBin(process.argv))
+  .scriptName("funko-app")
+  .usage("$0 <cmd> [args]")
+  .help()
+  .argv;
